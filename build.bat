0adbc0e49056289f4fd62fd29f5bf3f3b81cbbb7
Bob Brown <bob@bigleafdesigns.com>
2020-09-09 13:34:51 -0600

    Removing boost from builds

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-cd ext\boost\
-call bootstrap.bat
-b2.exe --with-system link=static threading=multi toolset=msvc address-model=64
-b2.exe --with-thread link=static threading=multi toolset=msvc address-model=64
-b2.exe --with-log link=static threading=multi toolset=msvc address-model=64
-cd ..\..\
+rem cd ext\boost\
+rem call bootstrap.bat
+rem b2.exe --with-system link=static threading=multi toolset=msvc address-model=64
+rem b2.exe --with-thread link=static threading=multi toolset=msvc address-model=64
+rem b2.exe --with-log link=static threading=multi toolset=msvc address-model=64
+rem cd ..\..\
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cd ext/boost/; ./bootstrap.sh;
-cd ext/boost/; ./b2 --with-system link=static threading=multi toolset=darwin cxxflags="-arch x86_64" target-os=darwin address-model=32_64
-cd ext/boost/; ./b2 --with-thread link=static threading=multi toolset=darwin cxxflags="-arch x86_64" target-os=darwin address-model=32_64
-cd ext/boost/; ./b2 --with-log link=static threading=multi toolset=darwin cxxflags="-arch x86_64" target-os=darwin address-model=32_64
+#cd ext/boost/; ./bootstrap.sh;
+#cd ext/boost/; ./b2 --with-system link=static threading=multi toolset=darwin cxxflags="-arch x86_64" target-os=darwin address-model=32_64
+#cd ext/boost/; ./b2 --with-thread link=static threading=multi toolset=darwin cxxflags="-arch x86_64" target-os=darwin address-model=32_64
+#d ext/boost/; ./b2 --with-log link=static threading=multi toolset=darwin cxxflags="-arch x86_64" target-os=darwin address-model=32_64
