f9c6e5e81ef241d24373b6d017e994edd7b7d75d
BobBrownConsulting <bob@bigleafdesigns.com>
2023-12-20 15:18:26 -0700

    Adding Dante category

--- src/utils/Trace.h
+++ src/utils/Trace.h
@@ -160,6 +160,7 @@ public:
         , kCategory_MeterMeasurements   = 0x000000000000000B
         , kCategory_Configuration       = 0x000000000000000C
         , kCategory_TI                  = 0x000000000000000D
+        , kCategory_Dante               = 0x000000000000000E
 
         , kCategory_Always              = 0x0FFFFFFFFFFFFFFF
     };
@@ -247,6 +248,7 @@ if (iCategory == iVal) \
         CATEGORY_TO_STRING(kCategory_MeterMeasurements);
         CATEGORY_TO_STRING(kCategory_Configuration);
         CATEGORY_TO_STRING(kCategory_TI);
+        CATEGORY_TO_STRING(kCategory_Dante);
         
         CATEGORY_TO_STRING(kCategory_Always);
 
@@ -286,7 +288,8 @@ if (0 == strcmp(iStr.c_str(), STRINGIFY(iVal))) \
         STRING_TO_CATEGORY(kCategory_MeterMeasurements);
         STRING_TO_CATEGORY(kCategory_Configuration);
         STRING_TO_CATEGORY(kCategory_TI);
-        
+        STRING_TO_CATEGORY(kCategory_Dante);
+
         STRING_TO_CATEGORY(kCategory_Always);
 
         BBC_ASSERT(!"stringToCategory - unknown iStr!");
